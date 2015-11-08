b3a0630f373daf20f9fd68b79f1b950e140bf169
olawar <warzecha.ola@gmail.com>
2015-11-08 01:11:17 +0100

    Update in the gitignore file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
-var debug = require('gulp-debug');
 
 gulp.task('default', function () {
 
     return gulp.src('css/*.css')
-        .pipe(debug({minimal: false}))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
